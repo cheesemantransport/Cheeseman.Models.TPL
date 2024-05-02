1c2f3d8b7312a1843bc4acebc5c118c9148b62c8
Rich Wood <rwood@cheeseman.com>
2024-05-02 13:19:20 -0400

    Added Pick Up and Delivery Appt Required flags to trigger Apptointment Fee accessorials where applicable.

--- Quote/LTLQuoteRequestModel.cs
+++ Quote/LTLQuoteRequestModel.cs
@@ -14,10 +14,16 @@ namespace Cheeseman.Models.TPL.Quote
         [Required]
         public string START_ZONE { get; set; }
 
+        [Description("Pick-Up Appointment required?")]
+        public bool PICK_UP_APPT_REQ { get; set; } = false;
+
         [Description("Consignee's US 5-digit Zip Code or Canadian 7-character Postal Code.")]
         [MaxLength(10)]
         [Required]
         public string END_ZONE { get; set; }
+
+        [Description("Delivery Appointment required?")]
+        public bool DELIVERY_APPT_REQ { get; set; } = false;
 
         [Description("Collection of freight details.")]
         [Required]
